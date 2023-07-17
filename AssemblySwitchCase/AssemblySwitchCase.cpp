336774a5bea464d5cea3ff12a4808f20a440d67f
김주영 <robolab1902@ringgames.com>
2023-07-17 11:45:55 +0900

    switch case 2만 늘린채 출력

--- AssemblySwitchCase/AssemblySwitchCase.cpp
+++ AssemblySwitchCase/AssemblySwitchCase.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+    int message = 0;
+    int response = 0;
+
+    switch (message)
+    {
+    case 0:
+        response = 0;
+        break;
+    case 2:
+        response = 1;
+        break;
+    case 4:
+        response = 2;
+        break;
+    case 6:
+        response = 3;
+        break;
+    }
 }
