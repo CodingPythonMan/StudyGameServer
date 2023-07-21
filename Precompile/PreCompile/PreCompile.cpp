8089cd193de778bce054033b8ecad18f29e96e5d
CodingPythonMan <robolab1902@naver.com>
2023-07-21 13:54:34 +0900

    Message Type 을 함수 포인터로 구현

--- Precompile/PreCompile/PreCompile.cpp
+++ Precompile/PreCompile/PreCompile.cpp
@@ -1,10 +1,22 @@
 ﻿#include "stdafx.h"
 
-#define MAX 100
-
+void Proc_0(){}
+void Proc_1(){}
+void Proc_2(){}
+void Proc_3(){}
+void Proc_4(){}
+void Proc_5(){}
 
 int main()
 {
-    int a = MAX;
-    std::cout << "Hello World!\n";
+	int MessageType = 0;
+	void (*ProcTable[6])(void);
+	ProcTable[0] = Proc_0;
+	ProcTable[1] = Proc_1;
+	ProcTable[2] = Proc_2;
+	ProcTable[3] = Proc_3;
+	ProcTable[4] = Proc_4;
+	ProcTable[5] = Proc_5;
+
+	ProcTable[MessageType]();
 }
