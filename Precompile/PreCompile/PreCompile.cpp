5f276cd69a9da97e7b36016229e1e0d970c4895a
CodingPythonMan <robolab1902@naver.com>
2023-07-21 14:31:48 +0900

    공부

--- Precompile/PreCompile/PreCompile.cpp
+++ Precompile/PreCompile/PreCompile.cpp
@@ -1,22 +1,16 @@
 ﻿#include "stdafx.h"
 
-void Proc_0(){}
-void Proc_1(){}
-void Proc_2(){}
-void Proc_3(){}
-void Proc_4(){}
-void Proc_5(){}
+void printString(char* p)
+{
+	p[0] = ' ';
+	printf(p);
+}
 
 int main()
 {
-	int MessageType = 0;
-	void (*ProcTable[6])(void);
-	ProcTable[0] = Proc_0;
-	ProcTable[1] = Proc_1;
-	ProcTable[2] = Proc_2;
-	ProcTable[3] = Proc_3;
-	ProcTable[4] = Proc_4;
-	ProcTable[5] = Proc_5;
+	int a = 5;
+	char string[10];
+	char sssss[] = "22222";
 
-	ProcTable[MessageType]();
+	printString(sssss);
 }
