1ddd260009aeca5274cf60c353b1335f907c1fb5
CodingPythonMan <robolab1902@naver.com>
2023-07-24 13:45:06 +0900

    rand 값 넣어서 해봄

--- Basic/BasicStruct/BasicStruct.cpp
+++ Basic/BasicStruct/BasicStruct.cpp
@@ -1,22 +1,19 @@
 ﻿#include <iostream>
 
-struct stDATA {
-    int a;
-    int b;
-    int c;
-};
-
 int main()
 {
-    stDATA data;
-    data.a = 0;
-    data.b = 0;
-    data.c = 0;
+    int r;
 
-    int x = data.c + 1;
-    
-    stDATA* p = &data;
-    p->a = 0;
-    p->b = 0;
-    p->c = 0;
+	srand(10);
+
+    r = rand();
+    printf("%d \n", r);
+	r = rand();
+	printf("%d \n", r);
+	r = rand();
+	printf("%d \n", r);
+	r = rand();
+	printf("%d \n", r);
+	r = rand();
+	printf("%d \n", r);
 }
