dbc501b6e168b8b96f2f808c3d28286cdadec2a4
CodingPythonMan <robolab1902@naver.com>
2023-07-24 12:45:35 +0900

    구조체 간단하게

---
+++ Basic/BasicStruct/BasicStruct.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+struct stDATA {
+    int a;
+    int b;
+    int c;
+};
+
+int main()
+{
+    stDATA data;
+    data.a = 0;
+    data.b = 0;
+    data.c = 0;
+
+    int x = data.c + 1;
+    
+    stDATA* p = &data;
+    p->a = 0;
+    p->b = 0;
+    p->c = 0;
+}
