2b450c68f70e72ffeaf4d55f55ef1222c7ee454c
robo <robolab1902@naver.com>
2023-07-26 08:27:53 +0900

    Practice 에서 c언어 rand, srand 구현해보기

---
+++ Practice/Rand/Rand/Rand.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    srand(100);
+
+    int x = rand();
+    printf("%d\n", x);
+    x = rand();
+    printf("%d\n", x);
+    x = rand();
+    printf("%d\n", x);
+    x = rand();
+    printf("%d\n", x);
+}
+
+int custom_rand()
+{
+
+}
