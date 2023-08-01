1f9bde650a367f178464878349d6d0736535b553
JuYoung Kim <robolab1902@naver.com>
2023-08-01 16:09:54 +0900

    Bit 연산자 과제에서 Change Bit 완료

--- Assembly/AssemblyBit/AssemblyBit.cpp
+++ Assembly/AssemblyBit/AssemblyBit.cpp
@@ -14,38 +14,50 @@ void PrintBit(unsigned char a)
 
 void ChangeBit()
 {
-    unsigned short a = 0;
+    unsigned short value = 0;
+    unsigned short temp;
+
+    int bitSize = sizeof(value) * 8;
+
     while (true)
     {
         int pos = 0;
+        int input = 0;
         printf("비트위치 : ");
         scanf_s("%d", &pos);
-        if (pos <= 0 || pos > 16)
+        if (pos <= 0 || pos > bitSize)
         {
             printf("값이 잘 못 입력되었습니다.");
             continue;
         }
-        int input = 0;
+        
         printf("OFF/ON [0,1] : ");
         scanf_s("%d", &input);
 
-        if (input == 0)
+        if (input != 0)
         {
-            a = a & (0 << pos);
-        }
-        else if(input == 1)
-        {
-            a = a | (1 << pos);
+            value = value | (1 << (pos-1));
         }
         else
         {
-            continue;
+            temp = ~(1 << (pos-1));
+            value = value & temp;
         }
 
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < bitSize; i++)
         {
-            printf("%d의 자리 : %d\n", i + 1, (a & 1 <<(16-i)) >> (16-i));
+            temp = value & (1 << (bitSize - i - 1));
+            
+            printf("%d 번 Bit : ", bitSize - i);
+            
+            if (temp != 0)
+                printf("ON");
+            else
+                printf("OFF");
+
+            printf("\n");
         }
+        printf("\n");
     }
 }
 
