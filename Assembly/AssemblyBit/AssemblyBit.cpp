8290cf1a08b5f20e08c0e05a0ea30e8a8984d303
김주영 <robolab1902@ringgames.com>
2023-08-01 19:06:13 +0900

    전혀 원하던 bit 출력 처럼 나오지 않았다

--- Assembly/AssemblyBit/AssemblyBit.cpp
+++ Assembly/AssemblyBit/AssemblyBit.cpp
@@ -63,22 +63,61 @@ void ChangeBit()
 
 void SharedBit()
 {
-    unsigned int a = 0;
+    unsigned int value = 0;
+    unsigned int temp;
+
+    int byteSize = sizeof(value);
+    int bitSize = byteSize * 8;
 
     for (;;)
     {
+        unsigned int mask = 0xFFFFFF00;
 		int pos = 0;
         printf("바이트 위치 : ");
 		scanf_s("%d", &pos);
 
-        if (pos < 1 || pos > 4)
+        if (pos < 1 || pos > byteSize)
         {
-            printf("잘못된 값이 입력 되었습니다.");
+            printf("잘못된 값이 입력 되었습니다.\n");
             continue;
         }
 
         printf("0~255 : ");
-        
+        scanf_s("%d", &temp);
+
+        if (temp < 1 || temp > 255)
+        {
+			printf("잘못된 값이 입력 되었습니다.\n");
+			continue;
+        }
+
+        mask = mask << (pos - 1);
+        value = value & mask;
+        temp = temp << ((pos - 1) * 8);
+        value = value | temp;
+
+        // Bit 단위 출력
+        printf("Bit : ");
+        for (int i = 0; i < bitSize; i++)
+        {
+            temp = value & (1 << (bitSize - i - 1));
+
+            if (temp != 0)
+                printf("%d", 1);
+            else
+                printf("%d", 0);
+        }
+
+        printf("\nByte : 0x");
+        // Byte 단위 출력
+        for (int i = 0; i < byteSize; i++)
+        {
+            mask = 0x000000FF;
+            temp = value & (mask << (byteSize - i - 1));
+            unsigned char a = temp >> (byteSize - i - 1);
+            printf("%x", a);
+        }
+        printf("\n");
     }
 }
 
@@ -87,5 +126,6 @@ int main()
     //unsigned char a = 101;
 
     //PrintBit(a);
-    ChangeBit();
+    //ChangeBit();
+    SharedBit();
 }
