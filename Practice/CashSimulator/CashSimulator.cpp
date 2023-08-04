993f83a0d8be1ffc63f47b51906f25317cf97e4e
JuYoung Kim <robolab1902@naver.com>
2023-08-04 11:46:18 +0900

    캐시 시뮬레이터 어떻게 만들어야 하나 이것저것 고민중

--- Practice/CashSimulator/CashSimulator.cpp
+++ Practice/CashSimulator/CashSimulator.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 
+struct Way {
+	int cashLine;
+};
+
+struct Cache {
+    Way way[8];
+};
+
+Cache cache[64];
+
 void CashSimulate(int* pointer)
 {
+    printf("%p\n", pointer);
 
+    //printf("Cache Hit!");
+
+    //printf("Cache Miss");
 }
 
 int main()
 {
-    
+    int* pointer;
+
+    int a = 0;
+    pointer = &a;
+    CashSimulate(pointer);
+    int b = 0;
+
+    a++;
+    pointer = &a;
+    CashSimulate(pointer);
+    b++;
+
+    a = b + 1;
+    pointer = &a;
+    CashSimulate(pointer);
 }
