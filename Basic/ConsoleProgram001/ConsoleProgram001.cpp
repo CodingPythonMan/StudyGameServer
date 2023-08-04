53a02a265d62494d095efda649a70e1f53e051c7
CodingPythonMan <robolab1902@naver.com>
2023-08-04 13:49:38 +0900

    Sleep 으로 커밋크기 보기

---
+++ Basic/ConsoleProgram001/ConsoleProgram001.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    int* p = (int*)malloc(1000000);
+
+    Sleep(1000);
+    for (int i = 0; i < 1000000; i++)
+    {
+        Sleep(1);
+        p[i] = 0;
+    }
+}
+ 
