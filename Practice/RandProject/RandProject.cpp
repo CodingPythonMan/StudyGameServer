a0b8648b16c4df57099741daac3f0cd447db149e
JuYoung Kim <robolab1902@naver.com>
2023-08-09 11:44:34 +0900

    두번째 랜덤 문제 풀고 있었는데 답이 틀림. 로직 보고 수정 필요

--- Practice/RandProject/RandProject.cpp
+++ Practice/RandProject/RandProject.cpp
@@ -3,44 +3,91 @@
 
 struct st_ITEM
 {
-    char Name[30];
-    int Rate;
+	char	Name[30];
+	int	Rate;		// 일반 랜덤 뽑기와 같은 용도
+	int	WinTime;	// 이 아이템이 나올 뽑기 회차.
+	// 0 이면 일반 아이템
+	// 0 이 아니면 그 회차에만 나옴
 };
 
 st_ITEM g_Gatcha[] = {
-	{"칼",		20},
-	{"방패",		20},
-	{"신발",		20},
-	{"물약",		20},
-	{"초강력레어무기",	5},
-	{"초강력방패",		5},
-	{"초강력레어레어레어신발", 1}
+	{"칼",				20, 0},
+	{"방패",				20, 0},
+	{"신발",				20, 0},
+	{"물약",				20, 0},
+	{"초강력레어무기",		5, 0},
+	{"초강력방패",			5, 0},
+	{"초강력레어레어레어신발1", 1, 50},
+	{"초강력레어레어레어신발2", 1, 51},
+	{"초강력레어레어레어신발3", 1, 10}
+
+	// 마지막 3개의 아이템은 일반 확률로는 나오지 않으며
+	// 뒤에 입력된 WinTime 회차때만 100% 로 나옴.
 };
+
+// 뽑기의 회차가 누적 계산이 되고 있어야 함.
+int iCount = 0;
 
 void Gatcha()
 {
-	// 1. 전체 아이템들의 비율 총 합을 구함
+	// 뽑기 회차 증가. (이는 전역적으로 계산 되어야 함)
+	iCount++;
+
+	// 1. 전체 아이템들의 비율 총 합을 구함.
+	// 단, WinTime 이 정해진 아이템은 확률의 대상 자체가 아니기 때문에 제외.
 	int total = 0;
-	int index = 0;
+	int max = 0;
 	for (const auto& item : g_Gatcha)
 	{
+		if (item.WinTime > 0)
+		{
+			if (max < item.WinTime)
+			{
+				max = item.WinTime;
+			}
+			continue;
+		}
+		
 		total += item.Rate;
 	}
 
-	// 2. rand() 함수로 확률을 구함
-	// 여기서 확률은 1/100 이 아니며, 1/총합비율임
+	// 2. 본 뽑기 회차에 대한 지정 아이템이 있는지 확인
+	// WinTime 과 iCount 가 같은 아이템을 찾는다.
+	// 있다면..그 아이템을 뽑고 중단.
+	for (const auto& item : g_Gatcha)
+	{
+		if (item.WinTime == iCount)
+		{
+			if (iCount == max)
+			{
+				iCount = 0;
+			}
 
-	// int iRand = 확률 구하기
+			printf("%d 가차 결과 : %s", iCount, item.Name);
+			return;
+		}
+	}
+
+	// 3. rand() 함수로 확률을 구함
+	// 여기서 확률은 1 / 100 이 아니며, 1 / 총합비율 임.
+
+	// int iRand = 확률구하기;
 	int random = (rand() % total) + 1;
 
-	// 3. 전체 아이템 테이블을 돌면서
+	// 4. 전체 아이템 테이블을 돌면서
 	// 위에서 구한 Rand 값에 해당 지점의 아이템을 찾는다.
-	while (random > g_Gatcha[index].Rate)
+	for (const auto& item : g_Gatcha)
 	{
-		random -= g_Gatcha[index].Rate;
-		index++;
+		if (random < item.Rate)
+		{
+			printf("%d 가차 결과 : %s\n", iCount, item.Name);
+			break;
+		}
+
+		random -= item.Rate;
 	}
-	printf("가챠 결과 : %s\n", g_Gatcha[index].Name);
+
+	// 5. 뽑기 회차를 초기화 해야할지 판단하여 초기화.
 }
 
 
