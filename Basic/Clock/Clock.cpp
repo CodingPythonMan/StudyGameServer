211a87ece2a3061c2af0c2645e59455d15497700
CodingPythonMan <robolab1902@naver.com>
2023-08-09 13:41:10 +0900

    Clock 등 타이머 이해 업로드

---
+++ Basic/Clock/Clock.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#pragma comment(lib, "winmm.lib")
+
+DWORD attackTick;
+
+void Attack()
+{
+    if (GetTickCount() - attackTick < 300)
+        return;
+
+    attackTick = GetTickCount();
+}
+
+int main()
+{
+    timeBeginPeriod(1);
+
+    GetTickCount();
+
+    while (1)
+    {
+        DWORD t1 = timeGetTime();
+        DWORD t2 = GetTickCount();
+
+        printf("%d / %d \n", t1, t2);
+    }
+    clock();
+}
