d144921d4de6691f958f82c23956ae9bed673f59
JuYoung Kim <robolab1902@naver.com>
2023-08-14 18:53:10 +0900

    타이밍 게임 과제 해결 중

--- Practice/Timing/Timing.cpp
+++ Practice/Timing/Timing.cpp
@@ -1,20 +1,82 @@
-// Timing.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+int g_Timing[] = { 5, 10, 14, 17, 20, 25, 29, 31, 33 };
+
+// 타이밍 데이터
+// 뒤로 갈 수록 큰 값을 넣는게 규칙
+
+// # 화면 지우기 system("cls")
+
+// # 시간구하기 - clock() 함수
+// 프로세스가 시작된 후의 시간, 1초는 CLOCKS_PER_SEC 값 (1000ms)
+// 리턴값은 clock_t (long) 타입
+
+// # 키보드 눌렸는지 확인 - _kbhit() 함수
+// 키보드가 눌렸는지 확인 후 눌렸으면 true 리턴
+// 단, 키를 누른 후 콘솔키에서 값을 빼주지 않으면 다음 루프에서도 계속 true 리턴
+
+// _getch() 함수를 사용하여 콘솔키 입력을 뽑을 수 있음.
+// 본래 사용법 char keyChar = _getch();
+// 우리는 _getch() 의 리턴값을 사용하지 않지만 콘솔키를 빼기 위해 씀.
+
+// # abs(X) 절대값 함수
+
+// - 컨텐츠 부
+// 1. 화면 상단에 시간이 표시 됨. 초 : 밀리세컨드 (00:000 으로 자리수 맞춤)
+// 2. 아래에는 각 키 타이밍의 정보와 해당 타이밍에 성공여부 결과를 표시
+// 3. 아무런 키를 누르지 않고 지정 시간을 1초 이상 넘으면 자동으로 fail 처리
+// 4. 사용자가 키를 누르면 해당 시간을 체크하여 오차 범위에 따라서 지정 타이밍의 결과가 화면에 표시됨.
+
+// CLOCKS_PER_SEC / 4 한 수치를 1단계로 최대 4단계 까지의 오차로 Great, Good, NoGood, Bad 단계 측정
+// 오차라는 것은, + - 모두 해당됨.
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int timingSize = sizeof(g_Timing) / sizeof(int);
+	double timing[timingSize];
+	char* grade[timingSize];
+	for (int i = 0; i < timingSize; i++)
+	{
+		timing[i] = g_Timing[i];
+	}
+
+	int index = 0;
+
+    while (1)
+    {
+		// 입력부
+		clock_t time = clock();
+		double sec = static_cast<double>(time) / CLOCKS_PER_SEC;
+		
+		if (_kbhit())
+		{
+			_getch();
+			// 컨텐츠 로직
+			timing[index] = sec;
+			index++;
+			
+			if (timingSize <= index)
+			{
+				break;
+			}
+		}
+
+		// 로직부
+		for (int i = 0; i < timingSize; i++)
+		{
+			double g_time = 
+			abs(1);
+		}
+		
+
+		// 랜더부
+		printf("%06.3f Sec\n\n", sec);
+		for (const int time : g_Timing)
+		{
+			printf("%d Sec : \n", time);
+		}
+		system("cls");
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
