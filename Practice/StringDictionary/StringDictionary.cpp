324ee8e3eb5b94d55bf7606f3054700cca5fed19
JuYoung Kim <robolab1902@naver.com>
2023-08-17 19:00:40 +0900

    2. strcpy 구현하다가 넘겼는데 왜 char* 를 반환하는지 알아보아야한다.

--- Practice/StringDictionary/StringDictionary.cpp
+++ Practice/StringDictionary/StringDictionary.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
-// 3. strcmp - 문자열 비교
-// 4. strcat - 문자열 붙이기
-// 5. strchr - 문자열 내에서 특정 문자 검색 (포인터 리턴)
-// 6. strstr - 문자열 내에서 특정 문자열 검색 (포인터 리턴)
-// 7. strlwr - 문자열을 소문자로 변환
-
 // 1. strlen - 문자열 길이 계산
 int my_strlen(char* str)
 {
@@ -23,12 +17,28 @@ int my_strlen(char* str)
 // 2. strcpy - 문자열 복사
 char* my_strcpy(char *strDest, const char *strSrc)
 {
+    while (*strSrc != '\0')
+    {
+        *strDest = *strSrc;
+        strDest++;
+        strSrc++;
+    }
 
+    return strDest;
 }
+
+// 3. strcmp - 문자열 비교
+// 4. strcat - 문자열 붙이기
+// 5. strchr - 문자열 내에서 특정 문자 검색 (포인터 리턴)
+// 6. strstr - 문자열 내에서 특정 문자열 검색 (포인터 리턴)
+// 7. strlwr - 문자열을 소문자로 변환
 
 int main()
 {
-    char name[50] = "ahhhha123";
+    char name[50] = "12312435";
+    char name2[50];
+
+    my_strcpy(name2, name);
     
-    printf("%d\n", my_strlen(name));
+    printf("%d\n", my_strlen(name2));
 }
