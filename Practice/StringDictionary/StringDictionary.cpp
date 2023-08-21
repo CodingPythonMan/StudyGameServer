2cbe977b7bcf2ffd1652192b56ddf15950aba99f
JuYoung Kim <robolab1902@naver.com>
2023-08-21 21:00:05 +0900

    strcmp 완성

--- Practice/StringDictionary/StringDictionary.cpp
+++ Practice/StringDictionary/StringDictionary.cpp
@@ -28,7 +28,27 @@ char* my_strcpy(char *strDest, const char *strSrc)
 }
 
 // 3. strcmp - 문자열 비교
+int my_strcmp(const char* string1, const char* string2)
+{
+    while (*string1 != '\0')
+    {
+        if (*string1 != *string2)
+        {
+            return 1;
+        }
+        string1++;
+        string2++;
+    }
+
+    return 0;
+}
+
 // 4. strcat - 문자열 붙이기
+char* my_strcat(char* strDest, const char* strAdd)
+{
+
+}
+
 // 5. strchr - 문자열 내에서 특정 문자 검색 (포인터 리턴)
 // 6. strstr - 문자열 내에서 특정 문자열 검색 (포인터 리턴)
 // 7. strlwr - 문자열을 소문자로 변환
@@ -39,6 +59,7 @@ int main()
     char name2[50];
 
     my_strcpy(name2, name);
+    int cmp = my_strcmp(name, name2);
     
-    printf("%d\n", my_strlen(name2));
+    printf("%d\n", cmp);
 }
