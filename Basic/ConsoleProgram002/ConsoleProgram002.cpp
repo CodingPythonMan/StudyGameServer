5c3dd782ba5f686bafa1a0d2f7191b07f90637be
CodingPythonMan <robolab1902@naver.com>
2023-08-25 14:22:01 +0900

    ConsoleProgram002 업로드

---
+++ Basic/ConsoleProgram002/ConsoleProgram002.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <timeapi.h>
+
+int main()
+{
+	int FrameCount = 0;
+	DWORD Tick = timeGetTime();
+
+	while (1)
+	{
+		FrameCount++;
+		if (timeGetTime() - Tick >= 1000)
+		{
+			printf("%d \n", FrameCount);
+			FrameCount = 0;
+			Tick = timeGetTime();
+		}
+
+		Sleep(20);
+	}
+}
