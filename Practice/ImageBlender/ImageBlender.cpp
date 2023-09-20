618c2b8486d127f85a4d5c5ccfe2f2e48a236bdb
JuYoung Kim <robolab1902@naver.com>
2023-09-20 11:43:39 +0900

    알파 블랜더 구현하는 중

--- Practice/ImageBlender/ImageBlender.cpp
+++ Practice/ImageBlender/ImageBlender.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+void AlphaBlending(char* bmp1, char* bmp2, char* result)
+{
+	FILE* file1;
+	FILE* file2;
+	FILE* fileResult;
+
+	file1 = fopen_s(&file1, );
+
+}
+
 int main()
 {
-
-
-    std::cout << "Hello World!\n";
+	AlphaBlending()
+   
 }
