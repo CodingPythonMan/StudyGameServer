fb830175c220781a6415909e5f73f49fa6071c60
robo <robolab1902@naver.com>
2023-09-29 15:14:04 +0900

    WinConsole003 을 통해 WinConsole002 의 우선순위를 낮추고  예제를 실행했다.

---
+++ Basic/WinConsole003/WinConsole003.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+int wmain(int argc, WCHAR* argv[])
+{
+    STARTUPINFO si = { 0, };
+    PROCESS_INFORMATION pi;
+    si.cb = sizeof(si);
+
+    TCHAR command[] = L"WinConsole002.exe";
+
+    CreateProcessW(
+        NULL, command, NULL, NULL, TRUE,
+        0, NULL, NULL, &si, &pi
+    );
+
+    DWORD timing = 0;
+    while (1)
+    {
+        for (DWORD i = 0; i < 10000; i++)
+            for (DWORD i = 0; i < 10000; i++)
+
+        fputws(L"Parent \n", stdout);
+
+        timing += 1;
+        if (timing == 2)
+            SetPriorityClass(pi.hProcess, NORMAL_PRIORITY_CLASS);
+    }
+
+    return 0;
+}
