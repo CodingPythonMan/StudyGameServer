5fd27969f0ec7fa014c70604eb5a58a071cf5146
robo <robolab1902@naver.com>
2023-10-01 16:32:25 +0900

    MailSlot 예제 업로드. 프로세스 별 통신에 대한 이해를 하며 예제를 작성해야한다

--- Basic/WinConsole001/WinConsole001.cpp
+++ Basic/WinConsole001/WinConsole001.cpp
@@ -2,17 +2,46 @@
 #include <tchar.h>
 #include <windows.h>
 
+// Receiver
+
+#define SLOT_NAME L"\\\\.\\mailslot\\mailbox"
+
 int wmain(int argc, WCHAR* argv[])
 {
-	STARTUPINFO si = { 0, };
-	PROCESS_INFORMATION pi;
-	si.cb = sizeof(si);
-	
-	WCHAR command[] = L"WinConsole002.exe";
-	CreateProcessW(NULL, command, NULL, NULL,
-		TRUE, 0, NULL, NULL, &si, &pi);
+	HANDLE hMailSlot;
+	WCHAR messageBox[50];
+	DWORD bytesRead; // number of bytes read
 
-	CloseHandle(pi.hProcess);
-	
+	// mailslot 생성
+	hMailSlot = CreateMailslot(SLOT_NAME, 0, MAILSLOT_WAIT_FOREVER, NULL);
+
+	if (hMailSlot == INVALID_HANDLE_VALUE)
+	{
+		fputws(L"Unable to create mailslot!\n", stdout);
+		return 1;
+	}
+
+	// Message 수신
+	fputws(L"***** Message *****\n", stdout);
+	while (1)
+	{
+		if (!ReadFile(hMailSlot, messageBox, sizeof(WCHAR) * 50, &bytesRead, nullptr))
+		{
+			fputws(L"Unable to read!", stdout);
+			CloseHandle(hMailSlot);
+			return 1;
+		}
+
+		if (!wcsncmp(messageBox, L"exit", 4))
+		{
+			fputws(L"Good Bye!", stdout);
+			break;
+		}
+
+		messageBox[bytesRead / sizeof(WCHAR)] = 0; // nullptr 삽입
+		fputws(messageBox, stdout);
+	}
+
+	CloseHandle(hMailSlot);
 	return 0;
 }
--- Basic/WinConsole002/WinConsole002.cpp
+++ Basic/WinConsole002/WinConsole002.cpp
@@ -2,16 +2,45 @@
 #include <tchar.h>
 #include <windows.h>
 
+// Sender
+
+#define SLOT_NAME L"\\\\.\\mailslot\\mailbox"
+
 int wmain(int argc, WCHAR* argv[])
 {
-	DWORD n = 0;
-	while (n < 100)
+	HANDLE hMailSlot;
+	WCHAR message[50];
+	DWORD bytesWritten; // number of bytes write
+
+	hMailSlot = CreateFile(SLOT_NAME, GENERIC_WRITE, FILE_SHARE_READ,
+		nullptr, OPEN_EXISTING, // 생성방식
+		FILE_ATTRIBUTE_NORMAL, nullptr);
+
+	if (hMailSlot == INVALID_HANDLE_VALUE)
 	{
-		for (DWORD i = 0; i < 10000; i++)
-			for (DWORD i = 0; i < 10000; i++) // Busy Waiting!!
-				fputws(L"WinConsole002.exe \n", stdout);
-		n++;
+		fputws(L"Unable to create mailslot!\n", stdout);
+		return 1;
 	}
 
+	while (1)
+	{
+		fputws(L"MY CMD>", stdout);
+		fgetws(message, sizeof(message)/sizeof(WCHAR), stdin);
+
+		if (!WriteFile(hMailSlot, message, wcslen(message) * sizeof(WCHAR), &bytesWritten, nullptr))
+		{
+			fputws(L"Unable to write!", stdout);
+			CloseHandle(hMailSlot);
+			return 1;
+		}
+
+		if (!wcscmp(message, L"exit"))
+		{
+			fputws(L"Good Bye!", stdout);
+			break;
+		}
+	}
+
+	CloseHandle(hMailSlot);
 	return 0;
 }
