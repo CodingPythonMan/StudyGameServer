2bfd52ca606011cc210e3b129af309c1d9c1662c
JuYoung Kim <robolab1902@naver.com>
2023-10-20 20:55:10 +0900

    5일차 내주신 과제 준비. 1. 도메인 IP 분산 리스트로 출력해보기, 2. listen socket 의 backlog 큐를 얼만큼 쌓을 수 있는지 실험해보기

--- Basic/newDelete/Main.cpp
+++ Basic/newDelete/Main.cpp
@@ -18,7 +18,20 @@ struct Player {
 
 int main()
 {
-	Player* p = new Player;
+	//Player* p = new Player;
 
-	delete[] p;
+	int a = -128;
+
+	const int* p1 = &a;
+	int const* p2 = &a;
+	int* const p3 = &a;
+
+	p1++;
+	p2++;
+	p3++;
+
+	//*p = 4;
+	*p2 = 5;
+	//*p3 = 7;
+	//p3++;
 }
