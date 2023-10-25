6d3f3ad97b8ef01c4229ae5a2965393efb089510
JuYoung Kim <robolab1902@naver.com>
2023-10-25 20:43:07 +0900

    프로파일러 작업 들어갔다. QueryPerformanceCounter, QueryPerformanceFreq 을 어떻게 이용해서 고해상도 측정하는지 시험했다

--- Practice/Profiler/MyProfiler.cpp
+++ Practice/Profiler/MyProfiler.cpp
@@ -1,20 +1,45 @@
 #include "MyProfiler.h"
+#include <stdio.h>
 
-void ProfileBegin(WCHAR* szName)
+LARGE_INTEGER Start;
+LARGE_INTEGER End;
+LARGE_INTEGER Freq;
+
+#define MAX_PROFILE 20
+
+ProfileInfo ProfileInfos[MAX_PROFILE];
+
+void ProfileInit()
+{
+	QueryPerformanceFrequency(&Freq);
+}
+
+void ProfileBegin(const WCHAR* szName)
 {
 	QueryPerformanceCounter(&Start);
+}
 
+void ProfileEnd(const WCHAR* szName)
+{
 	QueryPerformanceCounter(&End);
+
+	double deltaTime = (End.QuadPart - Start.QuadPart) / (double)Freq.QuadPart;
+	
+	for (int i = 0; i < MAX_PROFILE; i++)
+	{
+		if (ProfileInfos[i]._flag > 1 && wcscmp(szName, ProfileInfos[i]._name) == 0)
+		{
+
+		}
+	}
 }
 
-void ProfileEnd(WCHAR* szName)
+void ProfileDataOutText(const WCHAR* szFileName)
 {
-}
 
-void ProfileDataOutText(WCHAR* szFileName)
-{
 }
 
 void ProfileReset(void)
 {
+
 }
--- Practice/Profiler/Profiler.cpp
+++ Practice/Profiler/Profiler.cpp
@@ -1,7 +1,21 @@
 ﻿#include <iostream>
+#include "MyProfiler.h"
 
+int TotalSum(int a)
+{
+    int total = 0;
+    for (int i = 0; i < a; i++)
+        total += i;
+
+    return total;
+}
 
 int main()
 {
-    
+    ProfileInit();
+
+
+    ProfileBegin(L"TotalSum");
+    printf("total : %d\n", TotalSum(1000));
+    ProfileEnd(L"TotalSum");
 }
