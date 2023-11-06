93704a8c785d6a4b03f2ec00b0667ad5beb045b9
CodingPythonMan <robolab1902@naver.com>
2023-11-06 14:18:46 +0900

    Star Project 이제 시작한다

---
+++ Socket/StarClient/Protocol.h
@@ -0,0 +1,28 @@
+#pragma once
+struct AssignID {
+	int Type;
+	int ID;
+	int X;
+	int Y;
+};
+
+struct CreateStar {
+	int Type;
+	int ID;
+	int X;
+	int Y;
+};
+
+struct DeleteStar {
+	int Type;
+	int ID;
+	int X;
+	int Y;
+};
+
+struct MoveStar {
+	int Type;
+	int ID;
+	int X;
+	int Y;
+};
---
+++ Socket/StarClient/StarClient.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+#include <windows.h>
+#include "Protocol.h"
+
+SOCKET sock;
+
+int main()
+{
+
+    // 블락 논블락은 언제든 변할 수 있다.
+    // connect 후 논 블락.
+
+    while (1)
+    {
+
+        // 네트워크 : FD_SET ReadSet
+
+
+        // 송신 : 좌표 변경되었다면, MoveStar send 한다.
+    }
+}
