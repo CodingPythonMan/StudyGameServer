138092ed7f622eeb5ad67eed7dd6ccf728a9386a
JuYoung Kim <robolab1902@naver.com>
2023-11-17 19:55:24 +0900

    텍스트 파서 간단하게 만들어보는 중. 어떻게 다음 단어로 이동하고 어디 단어가 끝인지 알아채야한다

--- Practice/TextParser/Parser.cpp
+++ Practice/TextParser/Parser.cpp
@@ -1,11 +1,14 @@
 #include "Parser.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-Parser::Parser()
+Parser::Parser() : Version{ 0 }, ServerID{ 0 }, buffer{ nullptr}
 {
 }
 
 Parser::~Parser()
 {
+	free(buffer);
 }
 
 Parser* Parser::GetInstance()
@@ -16,10 +19,48 @@ Parser* Parser::GetInstance()
 
 void Parser::LoadFile(const char* fileName)
 {
+	FILE* file;
+	fopen_s(&file, fileName, "r+");
 
+	if (file == nullptr)
+		return;
+
+	fseek(file, 0, SEEK_END);
+	int length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	buffer = (char*)malloc(length + 1);
+	if (buffer == nullptr)
+		return;
+
+	fread_s(buffer, length + 1, 0, length, file);
+
+	fclose(file);
 }
 
 void Parser::GetValueInt(const char* valueName, int* column)
 {
+	
 
+
+}
+
+void Parser::GetNextWord(char* chrBuffer, int length)
+{
+	chrBuffer = buffer;
+
+	while (1)
+	{
+		// 0x08 백스페이스
+		// 0x09 탭
+		// 0x0a 라인 피드
+		// 0x0d 캐리지 리턴
+		if (*chrBuffer == ',' || *chrBuffer == '.' || *chrBuffer == '"' || *chrBuffer == 0x20
+			|| *chrBuffer == 0x08 || *chrBuffer == 0x09 || *chrBuffer == 0x0a || *chrBuffer == 0x0d)
+		{
+			break;
+		}
+
+		chrBuffer++;
+	}
 }
--- Practice/TextParser/Parser.h
+++ Practice/TextParser/Parser.h
@@ -6,11 +6,14 @@ private:
 	Parser();
 	~Parser();
 
+	char* buffer;
+
 public:
 	static Parser* GetInstance();
 
 	void LoadFile(const char* fileName);
 	void GetValueInt(const char* valueName, int* column);
+	void GetNextWord(char* chrBuffer, int length);
 
 	int Version;
 	int ServerID;
