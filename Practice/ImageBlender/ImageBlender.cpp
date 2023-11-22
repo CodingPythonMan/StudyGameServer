5fe9fcd0546f093cec11da043a38887daac2e874
JuYoung Kim <robolab1902@naver.com>
2023-11-22 11:20:36 +0900

    Image Blender 하기 전에 이미지 복사부터 잘 되는지 확인했다. 복사는 잘 된다

--- Practice/ImageBlender/ImageBlender.cpp
+++ Practice/ImageBlender/ImageBlender.cpp
@@ -34,18 +34,58 @@ void AlphaBlending(const char* bmp1, const char* bmp2, const char* result)
 	ret = (int)fread_s(&BitmapFileHeader, (int)sizeof(BitmapFileHeader), 1, (int)sizeof(BitmapFileHeader), file2);
 	if (ret != sizeof(BitmapFileHeader))
 	{
-		printf("File1 Read Error !!! \n");
+		printf("File2 Read Error !!! \n");
 		return;
 	}
 
 	if (BitmapFileHeader.bfType != 0x4D42)
 	{
-		printf("File1 Header Error!!! \n");
+		printf("File2 Header Error!!! \n");
 		return;
 	}
 
-	int imageSize = BitmapInfoHeader.biWidth * BitmapInfoHeader.biHeight;
+	ret = (int)fread_s(&BitmapInfoHeader, (int)sizeof(BitmapInfoHeader), 1, (int)sizeof(BitmapInfoHeader), file1);
+	if (ret != sizeof(BitmapInfoHeader))
+	{
+		printf("File1 Read Error !!! \n");
+		return;
+	}
 
+	ret = (int)fread_s(&BitmapInfoHeader, (int)sizeof(BitmapInfoHeader), 1, (int)sizeof(BitmapInfoHeader), file2);
+	if (ret != sizeof(BitmapInfoHeader))
+	{
+		printf("File2 Read Error !!! \n");
+		return;
+	}
+
+	// 픽셀 데이터 읽기
+	int imageSize = (BitmapInfoHeader.biWidth * BitmapInfoHeader.biHeight) * 4;
+	char* image1 = (char*)malloc(imageSize);
+	if (image1 == nullptr)
+		return;
+	ret = (int)fread_s(image1, imageSize, 1, imageSize, file1);
+
+	char* image2 = (char*)malloc(imageSize);
+	if (image2 == nullptr)
+		return;
+	ret = (int)fread_s(image2, imageSize, 1, imageSize, file2);
+
+	int fileResultSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + imageSize;
+	char* imageResult = (char*)malloc(fileResultSize);
+
+	if (imageResult == nullptr)
+		return;
+
+	memcpy(imageResult, &BitmapFileHeader, sizeof(BitmapFileHeader));
+	memcpy(imageResult + sizeof(BitmapFileHeader), &BitmapInfoHeader, sizeof(BitmapInfoHeader));
+	memcpy(imageResult + sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader), image2, imageSize);
+
+	// 픽셀 데이터 조절하기
+	fwrite(imageResult, 1, fileResultSize, fileResult);
+
+	free(image1);
+	free(image2);
+	free(imageResult);
 
 	fclose(file1);
 	fclose(file2);
