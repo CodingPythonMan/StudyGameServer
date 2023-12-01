f9f7fc3c0a79be8111fa3882792a2611e8642ad7
JuYoung Kim <robolab1902@naver.com>
2023-12-01 20:59:26 +0900

    Raw 만들고 있다2

--- Practice/MemoryPool/RawMemoryPool.cpp
+++ Practice/MemoryPool/RawMemoryPool.cpp
@@ -1,2 +1,19 @@
 #include "RawMemoryPool.h"
 
+RawMemoryPool::RawMemoryPool()
+{
+}
+
+RawMemoryPool::~RawMemoryPool()
+{
+}
+
+void* RawMemoryPool::Alloc(void)
+{
+	return nullptr;
+}
+
+bool RawMemoryPool::Free(void* pData)
+{
+	return false;
+}
