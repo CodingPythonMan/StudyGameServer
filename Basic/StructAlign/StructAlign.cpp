e364c7aa7714b8e268a63f1ae23386fdbdbd6878
JuYoung Kim <robolab1902@naver.com>
2024-01-09 17:44:31 +0900

    alignas 에 대한 개념을 대충 감을 잡았다. 이를 어디서 적혀있길래 확신으로 이야기할 수 있는 걸까?

--- Basic/StructAlign/StructAlign.cpp
+++ Basic/StructAlign/StructAlign.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 struct alignas(32) DATA1 {
 	char a;
 	short b;
 	int c;
-	long d;
+	int d[8];
 };
 
 struct DATA2 {
 	char a;
 	short b;
 	alignas(32) int c;
-	long d;
+	int d[7];
+};
+
+struct User {
+	char name[50];
+	int a;
+	int b;
+
+	User()
+	{
+		memset(name, 0, 50);
+		a = 12;
+		b = 30;
+	}
 };
 
 int Test(int a, int b)
@@ -25,5 +39,22 @@ int main()
 	cout << "DATA1 sizeof : " << sizeof(DATA1) << "\n";
 	cout << "DATA2 sizeof : " << sizeof(DATA2) << "\n";
 
+	DATA1 data1;
+	data1.a = 0;
+	data1.b = 0;
+	data1.c = 0;
+
+	DATA2 data2;
+	data2.a = 0;
+	data2.b = 0;
+	data2.c = 0;
+
 	//int test = Test(10, 20);
+	list<User*> userList;
+
+	User* user = new User;
+	userList.push_back(user);
+
+	for (auto iter = userList.begin(); iter != userList.end(); ++iter)
+		delete (*iter);
 }
