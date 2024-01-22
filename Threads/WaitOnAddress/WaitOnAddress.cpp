aa9a5427904826a2175acc9ae02b114525e2c2c5
JuYoung Kim <robolab1902@naver.com>
2024-01-22 19:04:51 +0900

    WaitOnAddress 를 이용한 동기화 구현도 깔끔하게 구현했다. 이제 더이상 걱정 안해도 된다.

--- Threads/WaitOnAddress/WaitOnAddress.cpp
+++ Threads/WaitOnAddress/WaitOnAddress.cpp
@@ -1,9 +1,53 @@
-// WaitOnAddress.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <process.h>
+using namespace std;
+
+#pragma comment(lib, "Synchronization.lib")
+
+// 0은 미사용
+long lock = 0;
+long AddVar = 0;
+
+void Lock(long* lock)
+{
+	long compare = 1;
+	while (1)
+	{
+		if (InterlockedExchange(lock, 1) == 1)
+			WaitOnAddress(lock, &compare, sizeof(long), INFINITE);
+		else
+			break;
+	}
+}
+
+void Unlock(long* lock)
+{
+	*lock = 0;
+	WakeByAddressSingle(lock);
+}
+
+unsigned int WINAPI AddFunc(LPVOID lpParam)
+{
+    Lock(&lock);
+
+    for (int i = 0; i < 1000000; i++)
+        AddVar++;
+
+    Unlock(&lock);
+
+    return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HANDLE threads[2];
+    threads[0] = (HANDLE)_beginthreadex(nullptr, 0, AddFunc, nullptr, 0, nullptr);
+    threads[1] = (HANDLE)_beginthreadex(nullptr, 0, AddFunc, nullptr, 0, nullptr);
+
+    WaitForMultipleObjects(2, threads, true, INFINITE);
+
+	printf("AddVar : %d\n", AddVar);
+
+	return 0;
 }
