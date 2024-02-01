2a3110eb59d3e08385cd2d196aab4ce3bf766ae4
robo <robolab1902@naver.com>
2024-02-01 21:51:23 +0900

    wstring 으로 랜덤 생성한 후, 이제 액터에서 워커스레드 설계해야한다.

--- Threads/ActorPattern/ActorPattern.h
+++ Threads/ActorPattern/ActorPattern.h
@@ -14,10 +14,24 @@ using namespace std;
 #define dfJOB_PRINT	4	//<< 출력 or 저장 / 읽기만 하는 느린 행동
 #define dfJOB_QUIT	5	
 
+//#pragma pack(push, 1)
 struct st_MSG_HEAD
 {
 	short shType;
 	short shPayloadLen;
 };
+//#pragma pack(pop)
 
 unsigned int WINAPI Work(LPVOID lpParam);
+
+//-----------------------------------------------
+// 컨텐츠 부, 문자열 리스트
+//-----------------------------------------------
+extern list<wstring>	gList;
+
+//-----------------------------------------------
+// 스레드 메시지 큐 (사이즈 넉넉하게 크게 4~5만 바이트)
+//-----------------------------------------------
+extern RingBuffer		messageQ;
+
+extern SRWLOCK lock;
--- Threads/ActorPattern/Main.cpp
+++ Threads/ActorPattern/Main.cpp
@@ -1,4 +1,18 @@
 #include "ActorPattern.h"
+#include <conio.h>
+
+#define RANDOM_MAX_LEN 30
+
+wstring GenerateRanString(short len)
+{
+	wstring tempData;
+	tempData.reserve(len);
+	
+	for (int i = 0; i < len; i++)
+		tempData.push_back('a' + rand() % 26);
+
+	return tempData;
+}
 
 int main()
 {
@@ -6,6 +20,42 @@ int main()
 	for (int i = 0; i < WORKER_THREAD_NUMBER; i++)
 	{
 		WorkerThreads[i] = (HANDLE)_beginthreadex(nullptr, 0, Work, nullptr, 0, nullptr);
+	}
+
+	while (1)
+	{
+		// L : 컨트롤 Lock / U : 컨트롤 Unlock / G : 서버 종료
+		if (_kbhit())
+		{
+			WCHAR ControlKey = _getwch();
+
+			// 키보드 제어 풀림 상태에서 특정 기능
+			if (L'q' == ControlKey || L'Q' == ControlKey)
+			{
+				wprintf(L"Control Mode : Press Q - Quit Send \n");
+			}
+		}
+
+		// 잡 랜덤 생성
+		short len = rand() % RANDOM_MAX_LEN;
+		st_MSG_HEAD head;
+		head.shType = rand() % dfJOB_QUIT;
+		head.shPayloadLen = len * 2;
+		wstring ranString = GenerateRanString(len);
+
+		if (messageQ.GetFreeSize() >= head.shPayloadLen + sizeof(st_MSG_HEAD))
+		{
+			AcquireSRWLockExclusive(&lock);
+
+			messageQ.Enqueue((char*)&head, sizeof(st_MSG_HEAD));
+			messageQ.Enqueue((char*)&ranString[0], head.shPayloadLen);
+
+			ReleaseSRWLockExclusive(&lock);
+		}
+		else
+			__debugbreak();
+
+		Sleep(50);
 	}
 
 	// 워커 스레드 종료 대기
