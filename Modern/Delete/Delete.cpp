9272cb6040848fab1967d8f0ba115776ac01ffb0
Kim Ju Young <robolab1902@naver.com>
2024-10-31 09:53:19 +0900

    어제 추가해줘야했던 WeakPtr 고친버전 업로드

---
+++ Modern/Delete/Delete.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <memory>
+
+class IInnerData
+{
+public:
+    IInnerData() = default;
+    virtual ~IInnerData()
+    {
+        Release();
+        //delete this;
+    }
+
+    virtual void Release() {};	// 해제해줘야 하는 항목 있으면 오버라이드
+};
+
+class spData : public IInnerData
+{
+public:
+    int X;
+    int Y;
+};
+
+class B
+{
+public:
+    int X;
+    int Y;
+
+    std::shared_ptr<IInnerData> pData;
+
+    B() 
+    {
+        pData = nullptr;
+        X = 0;
+        Y = 0;
+        printf("B::B()\n"); 
+    }
+
+    ~B() 
+    {
+        printf("B::~B()\n"); 
+    }
+};
+
+class C
+{
+
+};
+
+class D : public C
+{
+public:
+    std::shared_ptr<C> c;
+};
+
+int main()
+{
+    
+    for (int i = 0; i < 1000; i++)
+    {
+        std::shared_ptr<spData> data = std::make_shared<spData>();
+
+        std::shared_ptr<B> b = std::make_shared<B>();
+        b->pData = data;
+        b->pData = nullptr;
+    }
+}
--- Modern/WeakPtr/Main.cpp
+++ Modern/WeakPtr/Main.cpp
@@ -1,26 +1,72 @@
 ﻿#include <iostream>
 #include <memory>
+#include <vector>
 
-std::weak_ptr<int> gw;
+class World;
 
-void observe()
+class Player : public std::enable_shared_from_this<Player>
 {
-    std::cout << "gw.use_count() == " << gw.use_count() << "; ";
-    // we have to make a copy of shared pointer before usage:
-    if (std::shared_ptr<int> spt = gw.lock())
-        std::cout << "*spt == " << *spt << '\n';
-    else
-        std::cout << "gw is expired\n";
-}
-
-int main()
-{
+public:
+    Player(std::string name) : name(name) 
     {
-        auto sp = std::make_shared<int>(42);
-        gw = sp;
-
-        observe();
+        std::cout << "Player " << name << " created.\n";
     }
 
-    observe();
+    ~Player() 
+    {
+        std::cout << "Player " << name << " destroyed.\n";
+    }
+
+    void joinWorld(std::shared_ptr<World> world);
+
+    std::string name;
+};
+
+class World {
+public:
+    void addPlayer(std::shared_ptr<Player> player) 
+    {
+        players.push_back(player);
+    }
+
+    void printPlayers() 
+    {
+        for (const auto& player : players) 
+        {
+            if (auto p = player.lock())
+            {
+                std::cout << "Player in world: " << p->name << "\n";
+            }
+            else 
+            {
+                std::cout << "A player has been removed.\n";
+            }
+        }
+    }
+
+private:
+    std::vector<std::weak_ptr<Player>> players;
+};
+
+void Player::joinWorld(std::shared_ptr<World> world) 
+{
+    world->addPlayer(shared_from_this());
+}
+
+int main() {
+    std::shared_ptr<World> world = std::make_shared<World>();
+
+    {
+        std::shared_ptr<Player> player1 = std::make_shared<Player>("Alice");
+        player1->joinWorld(world);
+
+        std::shared_ptr<Player> player2 = std::make_shared<Player>("Bob");
+        player2->joinWorld(world);
+
+        world->printPlayers();
+    }
+
+    world->printPlayers();
+
+    return 0;
 }
