09add3f99884df10cd045760935ec6c1774023bf
Kim Ju Young <robolab1902@naver.com>
2025-01-17 14:29:20 +0900

    Move 이용시 복사 대신 이동 연산자가 호출되는 것 확인.

---
+++ Modern/Move/Move.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <utility>
+
+struct MyMap {
+    int a = 0;
+    MyMap() = default;
+
+    //// 이동 생성자
+    MyMap(MyMap&& other) noexcept {
+        std::exchange(a, other.a);
+        std::cout << "Move Constructor Called\n";
+    }
+
+    // 복사 생성자
+    MyMap(const MyMap& other) {
+        a = other.a;
+        std::cout << "Copy Constructor Called\n";
+    }
+};
+
+int main() {
+    std::unordered_map<int, MyMap> dualMap;
+    MyMap map;
+
+    map.a = 1;
+
+    // std::move 사용
+    dualMap.emplace(1, std::move(map));  // "Move Constructor Called" 출력
+
+    // std::move 미사용
+    MyMap map2;
+    map2.a = 2;
+    dualMap.emplace(2, map2);  // "Copy Constructor Called" 출력
+
+    return 0;
+}
