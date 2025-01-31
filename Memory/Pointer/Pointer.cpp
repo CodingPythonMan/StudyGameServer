e8f333d432dd1dae2e2c3d6c927e9a13acf7b06f
Kim Ju Young <robolab1902@naver.com>
2025-01-31 10:57:04 +0900

    VirtualAlloc 은 따로 작업한 내용이 없어 삭제하고, 포인터를 키로 쓰는 자료구조일 때 어떤 일이 일어나는지 확인

---
+++ Memory/Pointer/Pointer.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+class FSPC
+{
+public:
+    int X;
+    int Y;
+
+    void Print()
+    {
+        printf("Print X : d, Y : d\n");
+    }
+};
+
+int main()
+{
+    std::unordered_map<FSPC*, int> userMap;
+
+    FSPC* pc = new FSPC;
+
+    userMap.emplace(pc, 15);
+    delete pc;
+
+    int value = userMap[pc];
+    printf("User Value : %d\n", value);
+
+    for (auto user : userMap)
+    {
+        user.first->Print();
+    }
+}
